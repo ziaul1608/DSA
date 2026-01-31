8758fce6b4da58c68165435d8126b899ec348912
mdziaulmustafa <ziaulmustafa2005@gmail.com>
2026-01-31 20:25:03 +0530

    dsa coding

--- src/searching/binarySearchMethod2.java
+++ src/searching/binarySearchMethod2.java
@@ -2,18 +2,17 @@ package searching;
 
 public class binarySearchMethod2 {
     static boolean searching(int[] arr, int st, int end, int target) {
-        int n = arr.length;
-        while(st <= end) {
-            int mid = (st + end) / 2;
-            if(target == arr[mid]) {
-                return true;
-            } else if (target < arr[mid]) {
-                return searching(arr, st, mid-1, target);
-            } else {
-                return searching(arr, mid+1, end, target);
-            }
+        if(st <= end) return false; //base case
+
+        int mid = (st + end) / 2;
+        if(target == arr[mid]) {
+            return true;
+        } else if (target < arr[mid]) {
+            return searching(arr, st, mid-1, target); //sub-problems
+        } else {
+            return searching(arr, mid+1, end, target); //sub-problems
         }
-        return false;
+
     }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
