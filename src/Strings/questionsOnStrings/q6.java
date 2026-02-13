18120ad3287343930519bcb4f0402ecf07e0b05f
mdziaulmustafa <ziaulmustafa2005@gmail.com>
2026-02-13 19:23:47 +0530

    dsa coding

---
+++ src/Strings/questionsOnStrings/q6.java
@@ -0,0 +1,23 @@
+package Strings.questionsOnStrings;
+
+public class q6 {
+    public static void main(String[] args) {
+        String str = "aaabbbbccddde";
+        String ans = "" + str.charAt(0);
+        int count = 1;
+        for (int i = 1; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            char prev = str.charAt(i-1);
+            if(curr == prev) count++;
+            else {
+                if(count > 1)  ans += count;
+                count = 1;
+                ans += curr;
+            }
+        }
+
+        if(count > 1) ans += count;
+        System.out.println(str);
+        System.out.println(ans);
+    }
+}
